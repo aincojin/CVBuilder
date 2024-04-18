6b10787f23d914eeeb3762fd91ac8adea50ffa78
aiincogin <lavsyanovich@mail.ru>
2024-04-18 20:27:11 +0300

    Created a pipe to make select-cvas dynamic

---
+++ src/app/shared/pipes/select-items-display.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SelectItemsDisplayPipe } from './select-items-display.pipe';
+
+describe('SelectItemsDisplayPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SelectItemsDisplayPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
